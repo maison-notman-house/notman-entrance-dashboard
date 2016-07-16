0ac3d0593dde8b50e522336da3c2d051034abc65
Tomas Hrdlicka <tomas@hrdlicka.co.uk>
2016-07-16 16:21:06 -0400

    Adding logo for DeviceCard component

--- src/components/device-card.js
+++ src/components/device-card.js
@@ -3,13 +3,24 @@ import React from 'react';
 export default function DeviceCard({deviceData}) {
     var deviceCount = 0;
 
+    var style = {
+        float: 'right',
+        width: '100px',
+        fontSize: 'small'
+    };
+
     if (typeof deviceData !== 'undefined') {
         deviceCount = Object.keys(deviceData.devices).length;
     }
 
     return  <div className="DeviceCard Card">
-        <div>
-            <img className="DeviceCard--icon" src="/devices.png"/>
-            Current occupancy is {deviceCount}.</div>
-    </div>;
+                <div>
+                    <img className="DeviceCard--icon" src="/devices.png"/>
+                    Current occupancy is {deviceCount}.
+                    <div style={style}>
+                        <br /><br /><br /><br />
+                        Data provided by <img src="/logos/reelyactive.png" width="50%" height="50%" />
+                    </div>
+                </div>
+            </div>;
 }
