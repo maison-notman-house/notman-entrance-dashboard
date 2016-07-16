4aa528ce1a16905ea92dba2aeb978233d8fec26c
Andre Mas <andrejohn.mas@gmail.com>
2016-07-16 15:55:15 -0400

    Updated styles

--- src/components/events-card.js
+++ src/components/events-card.js
@@ -10,9 +10,23 @@ export default function EventsCard({events}) {
 }
 
 
+function createTitle(title) {
+   if (title && title.length > 50) {
+     title = title.substring(0,50) + '...';
+   }
+   return title;
+}
+
 function createDay(e) {
+//  let title = e.title;
+//   if (title && title.length > 60) {
+//      title = title.substring(0,60) + '...';
+//   }
+//   console.log('....... ' + e);
+//   console.log('....... ' + title);
+
   let items = e.items.map(e => {
-    return <div><span className="strong">{e.title}</span> <span className={"roomlabel " + e.tags.join(', ').replace(' ','') } >{e.tags.join(', ') }</span></div>;
+    return <div><span className="eventname">{createTitle(e.title)}</span> <span className={"roomlabel " + e.tags.join(', ').replace(' ','') } >{e.tags.join(', ') }</span></div>;
   });
 
   return <div className="EventsCard-day">
