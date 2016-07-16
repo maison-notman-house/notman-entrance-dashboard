e70f01b6334e671e730e3fc43fd5509d9effa7d4
Andre Mas <andrejohn.mas@gmail.com>
2016-07-16 15:10:04 -0400

    Adjusted room labels

--- src/components/events-card.js
+++ src/components/events-card.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
 export default function EventsCard({events}) {
-  
+
     events = events || [];
-    
+
     return <div className="EventsCard Card">
       {events.map(createDay)}
     </div>;
 }
 
+
 function createDay(e) {
   let items = e.items.map(e => {
-    return <div><span className="strong">{e.title}</span> {e.tags.join(', ') }</div>;
+    return <div><span className="strong">{e.title}</span> <span className={"roomlabel " + e.tags.join(', ').replace(' ','') } >{e.tags.join(', ') }</span></div>;
   });
-  
+
   return <div className="EventsCard-day">
     <div className="EventsCard-day-name">
       {e.day}
     </div>
-    
+
     {items}
   </div>;
 }
