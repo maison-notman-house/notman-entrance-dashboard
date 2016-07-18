bbc55dfde568a08dcfe26595a5d32e3025bd2413
Andre Mas <andrejohn.mas@gmail.com>
2016-07-18 14:07:13 -0400

    Tweaks to sponsors sections

--- src/components/sponsors-panel.js
+++ src/components/sponsors-panel.js
@@ -4,6 +4,10 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 const SPONSORS = [
   'bdc.jpg',
   'hydro-quebec.svg',
+  'google-for-partner.png',
+  'quickbooks_intuit.png',
+  'FTQ.jpg',
+  'videotron.svg'
 ];
 
 const INTERVAL = 6000;
@@ -11,29 +15,29 @@ const INTERVAL = 6000;
 export default class SponsorsPanel extends React.Component {
   constructor() {
     super();
-    this.state = { index: 0 };  
+    this.state = { index: 0 };
   }
-  
+
   componentDidMount() {
     this.intervalTimer = setInterval(this.update.bind(this), INTERVAL);
   }
-  
+
   componentWillUnmount() {
     clearInterval(this.intervalTimer);
   }
-   
+
   getCurrentSponsorImageUrl() {
     return '/logos/' + SPONSORS[this.state.index];
-  }  
-  
+  }
+
   update() {
-    let index = (this.state.index + 1) % SPONSORS.length; 
+    let index = (this.state.index + 1) % SPONSORS.length;
     console.log('Changed index to', index);
     this.setState({index});
   }
-  
-  render() {  
-    return <div className="Panel SponsorsPanel">
+
+  render() {
+    return <div className="SponsorsCard Card">
       <ReactCSSTransitionGroup transitionName="transition" transitionEnterTimeout={800} transitionLeaveTimeout={800}>
         <img className="sponsor-image" src={this.getCurrentSponsorImageUrl()} key={this.getCurrentSponsorImageUrl()} />
       </ReactCSSTransitionGroup>
