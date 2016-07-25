6a4b02b9bdaa0e3739065ebfd02236323e07019e
Andre Mas <andrejohn.mas@gmail.com>
2016-07-25 13:38:01 -0400

    Changed the video displayed

--- src/components/video-panel.js
+++ src/components/video-panel.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function () {
-    var url='https://www.youtube.com/embed/wa7uUX0l5ng?autoplay=1&loop=1';
-    var width='560';
-    var height='315';
+    var url='https://www.youtube.com/embed/MIe4TebZfaM?autoplay=1&loop=1';
+    var width='660';
+    var height='375';
     return <div className="VideoPanel">
               <iframe width={width} height={height} src={url} frameborder="0"></iframe>
            </div>;
