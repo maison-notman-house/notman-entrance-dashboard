e6802d641c79245d2c36a1b0ad83a784e0c6b967
Andre Mas <andrejohn.mas@gmail.com>
2016-07-27 17:31:13 -0400

    Current Weather now update every 15min
    Changed the code for the current-weather card, so that it gets
    updated every 15 minutes. This required moving the ajax call to
    the component.
    
    Also changed the full page refresh value. The value had mistakenly
    assumed it was in milliseconds.

--- src/components/currentweather-card.js
+++ src/components/currentweather-card.js
@@ -1,34 +1,57 @@
 import React from 'react';
 import Moment from 'moment';
 
-export default function CurrentWeatherCard({weatherData}) {
-    var cityName ='N/A';
-    var cityData = 'No data!';
+export default class CurrentWeatherCardComponent extends React.Component {
 
-    if (typeof weatherData !== 'undefined') {
-        cityName = weatherData.name;
-        cityData = displayData(weatherData);
-    }
+  updateWeatherData() {
+    var scope = this;
+    var data = fetch(this.fetchUrl).then(response => response.json()).then(function (data) {
+      scope.setState({
+        data: data
+      });
+    });
+  }
 
-    return  <div className="WeatherCard Card">
-                <h3>{cityName} (Current)</h3>
-                {cityData}
-            </div>;
-}
-
-function displayData(data) {
+  displayData(data) {
     var imageUri = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
     var imageDesc = data.weather[0].description;
     var date = Moment(data.dt_txt).locale("en").fromNow(false);
     var temp = Math.round(data.main.temp);
 
-    return  <div className="WeatherCard-day">
-                <div className="WeatherCard-day-name">
-                    <div className="weatherImg"><img alt={imageDesc} src={imageUri} /></div>
-                    <span className="temperature"> <b>{temp}</b></span>&#8451;
-                    <span className="humidity"> (Humidity: <b>{data.main.humidity}%</b>)</span>,&nbsp;
-                    <span className="conditionLabel">{data.weather[0].description}</span>
+    return <div className="WeatherCard-day">
+      <div className="WeatherCard-day-name">
+        <div className="weatherImg"><img alt={imageDesc} src={imageUri} /></div>
+        <span className="temperature"> <b>{temp}</b></span>&#8451;
+        <span className="humidity"> (Humidity: <b>{data.main.humidity}%</b>) </span>, &nbsp;
+        <span className="conditionLabel">{data.weather[0].description}</span>
+      </div>
+    </div>;
+  }
 
-                </div>
-            </div>;
+  componentWillMount() {
+    this.fetchUrl = 'http://api.openweathermap.org/data/2.5/weather?id=6077243&APPID=dc252e41ccdd53d06d044cde8f15dedb&units=metric&lang=en';
+    this.updateWeatherData();
+  }
+
+  componentDidMount() {
+    window.setInterval(function () {
+      this.updateWeatherData();
+    }.bind(this), 15000);
+  }
+
+  render() {
+    var cityName = 'N/A';
+    var cityData = 'No data!';
+
+    if (this.state && this.state.data) {
+      cityName = this.state.data.name;
+      cityData = this.displayData(this.state.data);
+    }
+
+    return <div className="WeatherCard Card">
+      <h3>{cityName} (Current) </h3>
+      {cityData}
+    </div>;
+  }
+
 }
