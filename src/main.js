f00236bccb82ee1ca1486a5c9b6df573364fda6c
Marek <marekz@gmail.com>
2016-08-20 17:37:28 +0000

    Add WebSockets and remote refresh capability

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './components/dashboard';
 
+import MessageClient from './sockets';
+
+const client = new MessageClient('ws://notman-marekweb.c9users.io:8080/socket');
+
 ReactDOM.render(<Dashboard/>, document.getElementById('container'));
