f8805f1854f22c0f8a382e7ede4139ee04e8d430
Marek <marekz@gmail.com>
2016-08-21 18:09:07 +0000

    Rename socket client and use correct socket url

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './components/dashboard';
 
-import MessageClient from './sockets';
+import MessageClient from './message-client';
 
-const client = new MessageClient('ws://notman-marekweb.c9users.io:8080/socket');
+const client = new MessageClient('ws://notman.herokuapp.com/socket');
 
 ReactDOM.render(<Dashboard/>, document.getElementById('container'));
