7293c21428d9ef0b753bccb2b9a156352593bd2c
Andre Mas <andrejohn.mas@gmail.com>
2016-08-30 12:08:01 -0400

    Branch for gh-pages
    Removed all files not necessary for gh-pages. Currently
    this branch is based on the product of the 'dist' folder
    create with the bin/create-dist.sh command in the
    'restucture' branch

--- src/components/logo-header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CurrentTime from './current-time';
-
-export default function LogoHeader() {
-  return <div className="LogoHeader">
-    <img src="images/notman-logo.svg"/>
-    <CurrentTime/>
-  </div>;
-}
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dashboard from './components/dashboard';
-
-import MessageClient from './message-client';
-
-//const client = new MessageClient('ws://notman.herokuapp.com/socket');
-
-ReactDOM.render(<Dashboard/>, document.getElementById('container'));
