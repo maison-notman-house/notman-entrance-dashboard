85f3df87970c9038266fc9d6c34065dad84ccafa
quinnlangille <quinn@picturesandwhatever.com>
2016-11-13 14:43:42 -0500

    banner styles

--- src/components/date-banner.js
+++ src/components/date-banner.js
@@ -10,8 +10,10 @@ export default function DateBanner({lang}) {
   const date = moment();
   
   // Why capitalize? Because in french, the weekday is lowercase by default.
-  const dateString = capitalize(date.locale(lang).format('dddd, DD MMMM YYYY'));
+  const dateString = capitalize(date.locale(lang).format('dddd, DD MMMM'));
   return <Banner>
+  	  <div><img src="../images/notman-logo.svg"/></div> 
+
       <div style={{textAlign: 'center', fontSize: '60px', lineHeight: '60px'}}>
         {dateString}
       </div>
