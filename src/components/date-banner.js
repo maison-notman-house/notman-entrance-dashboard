5c1ac98a7287fb42bb8d4b8820cc72fa04df31c4
Aengus <aengus.hearne@gmail.com>
2016-11-13 14:41:41 -0500

    Add time & logo to banner

--- src/components/date-banner.js
+++ src/components/date-banner.js
@@ -10,10 +10,17 @@ export default function DateBanner({lang}) {
   const date = moment();
   
   // Why capitalize? Because in french, the weekday is lowercase by default.
-  const dateString = capitalize(date.locale(lang).format('dddd, DD MMMM YYYY'));
+  const dateString = capitalize(date.locale(lang).format('dddd, DD MMMM'));
+  const time = capitalize(date.locale(lang).format(' HH:mm'));
   return <Banner>
-      <div style={{textAlign: 'center', fontSize: '60px', lineHeight: '60px'}}>
+  <div>
+  	 <div><img src="../images/notman-logo.svg"/></div>
+
+      <div style={{textAlign: 'right', fontSize: '40px', lineHeight: '60px'}}>
         {dateString}
+        {time} 
       </div>
+
+   </div>
   </Banner>
 }
