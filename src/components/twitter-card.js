92474fdee29b019004847cf98e7c890a2d9f4cf1
Andre Mas <andrejohn.mas@gmail.com>
2017-01-10 23:33:51 -0500

    Addition of Twitter card for hallway screens

---
+++ src/components/twitter-card.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Timeline} from 'react-twitter-widgets';
+
+/**
+ * Crates a Twitter card for the dashboard
+ */
+export default class TwitterCardComponent extends React.Component {
+
+    componentWillMount() {
+        this.refreshIntervalSeconds = 60;
+    }
+
+    componentDidMount() {
+        window
+            .setInterval(function () {
+                this.setState({x: 1});
+            }.bind(this), (this.refreshIntervalSeconds * 1000));
+
+    }
+
+    render() {
+
+        return (
+            <div className="Card">
+                <Timeline
+                    dataSource={{
+                        sourceType: 'profile',
+                        screenName: 'notman'
+                    }}
+                    options={{
+                        username: 'notman',
+                        height: '400',
+                        chrome: 'nofooter, noborders, transparent, noscrollbar'
+                    }}/>
+            </div>
+        );
+    }
+
+}
