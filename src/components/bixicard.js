72af224a6a2f933fdec82634d80e7413f4df37e8
aengushearne <aengus.hearne@gmail.com>
2017-06-26 19:49:53 -0400

    Fetch & display bikes at nearest bixi station. 1 min refresh.

--- src/components/bixicard.js
+++ src/components/bixicard.js
@@ -4,12 +4,13 @@ import LocalizedStrings from 'react-localization';
 let strings = new LocalizedStrings({
     en: {
         'theres': 'There are',
-        'available': 'Bixi bikes available'
+        'bikes': ' Bixi bicycle',
+        'available': ' available at Milton/Clark'
     },
     fr: {
         'theres': 'Il y a',
-        'available': 'vélo Bixi disponible'
-
+        'bikes': 'vélo',
+        'available': ' Bixi disponible à Milton/Clark'
     }
 });
 
@@ -18,25 +19,37 @@ export default class BixiCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [],
-            error: null
+            //data: [],
+            ba: 0,
         };
+        this.updateBixi = this.updateBixi.bind(this);
     }
 
     updateBixi() {
         fetch('https://secure.bixi.com/data/stations.json')
         .then(res => res.json())
         .then(res => {
-            this.setState({data: res.stations});
-            console.log('Bixi data '+this.state.data[6]);
+            //this.setState({data: res.stations});
+            res.stations.filter(function(station){
+                // milton/clark station 6209
+                return station.n === "6209";
+            }).map(function(station){
+                // set state of bikes available
+                this.setState({ba: station.ba});
+               // console.log('Bixi at', Date.now(), this.state.ba)
+            }, this);
         }).catch(err => {
-            this.setState({error: err});
-            console.log('Bixi error'+this.state.error);
+            console.log('Bixi error '+err);
         });
     }
 
     componentWillMount() {
         this.updateBixi();
+    }
+    componentDidMount() {
+        window.setInterval(function(){
+            this.updateBixi();
+        }.bind(this), 60000);
     }
 
     componentWillReceiveProps(nextProps) {
@@ -47,8 +60,10 @@ export default class BixiCard extends React.Component {
     render(){
         return(
             <div className='Card '>
-              {strings.theres} {strings.available}
-              
+                <img id='bixi-logo' src='images/logos/Bixi_logo.svg.png' alt='Bixi'/>
+                <div className='bixi-text'>
+                {this.state.ba} {strings.bikes}{this.state.ba === 1?'':'s'} {strings.available}
+                </div>
             </div>
         );
     }
